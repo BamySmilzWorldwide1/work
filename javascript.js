14965dcd32b390d0e791c971c687fbbfbf463d55
BamySmilzWorldwide1 <squeezytinzy@gmail.com>
2022-06-24 09:43:47 +0100

    javascrit complete task

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//Addition
+let x = 50;
+let y = 20;
+let z = x + y;
+
+console.log(z);
+
+// Subtraction
+let a = 90;
+let b = 60;
+let c = a - b;
+
+console.log(c)
+
+// Multiplication
+let o = 40;
+let p = 20;
+let q = o * p;
+
+console.log(q)
+
+// Division
+let r = 120;
+let u = 30;
+let v = r / u;
+
+console.log(v)
